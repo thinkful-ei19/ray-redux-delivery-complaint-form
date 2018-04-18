882b2b27837bef66dbd060820212ecccbcdd5425
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-04-18 11:13:52 -0700

    updated HTML form fields and updated validators

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import {reduxForm, Field, SubmissionError, focus} from 'redux-form';
 import Input from './input';
-import {required, nonEmpty, email} from '../validators';
+import {required, nonEmpty, fiveCharacters, nonNumber} from '../validators';
+import './complaint-form.css';
 
 export class ComplaintForm extends React.Component {
-    
+    onSubmit(values) {
+        
+    }
+    render() {
+        return (
+            <form 
+                onSubmit={this.props.handleSubmit(values =>
+                    this.onSubmit(values)
+                )}>
+                <label htmlFor="trackingnumber">Tracking number</label>
+                <Field 
+                    type="text" 
+                    name="tracking-number" 
+                    id="tracking-number"
+                    component={Input}
+                    validate={[required, nonEmpty, fiveCharacters, nonNumber]}
+                /> <br/>
+                <label htmlFor="issue">What is your issue?</label> <br/>
+                <Field 
+                    name="issue" 
+                    component="select"
+                    validate={[required]}
+                >
+                    <option value="">My delivery hasn't arrived</option>
+                    <option value="">The wrong item was delivered</option>
+                    <option value="">Part of my order was missing</option>
+                    <option value="">Some of my order arrived damaged</option>
+                    <option value="">Other (give details below)</option>
+                </Field> 
+                <br/>
+                <br/>
+                <label htmlFor="details">Give more details (optional)</label> <br/>
+                <Field 
+                    rows="4" cols="50" 
+                    name="comment" 
+                    form="userform"
+                    id="comment"
+                    component="textarea"
+                /> <br/>
+                <button type="submit">Submit</button> <br/>
+            </form>
+        );
+    }
 };
 
 export default reduxForm({
-
+    form: 'complaint'
 })(ComplaintForm);
--- src/validators.js
+++ src/validators.js
@@ -3,5 +3,8 @@ export const required = value => (value ? undefined : 'Required');
 export const nonEmpty = value =>
     value.trim() !== '' ? undefined : 'Cannot be empty';
 
-export const email = value =>
-    /^\S+@\S+$/.test(value) ? undefined : 'Must be a valid email address';
+export const fiveCharacters = value =>
+    value.length !== 5 ? undefined : 'Must be exactly 5 characters';
+
+export const nonNumber = value =>
+    typeof(value) !== "number" ? undefined : 'Must be numbers';
